d07ad8f994ba0323cdf9d912922989b4510445ad
gitadmen <94184870+gitadmen@users.noreply.github.com>
2022-03-11 11:23:43 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
     $('#btn').click(function() {
 
 
-        $.get('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
             .done(function(data) {
 
-                $('#dane-programisty').html('').append(data + "<br/>")
+                $('#dane-programisty').html('')
+                $('#dane-programisty').append("Imię: " + data.imie + "<br/>")
+                $('#dane-programisty').append("Nazwisko: " + data.nazwisko + "<br/>")
+                $('#dane-programisty').append("Zawód: " + data.zawod + "<br/>")
+                $('#dane-programisty').append("Firma: " + data.firma + "<br/>")
+                console.log(data)
 
             })
 
